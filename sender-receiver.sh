f7e23e55ad313815b3a945f4f04a94eb95e0dfea
spelekis <spelekis@epu.ntua.gr>
2020-06-26 15:25:48 +0300

    added sourcesonOff receiving service

--- sender-receiver.sh
+++ sender-receiver.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 D-ITG/bin/ITGRecv &> receiver.stdout &
-D-ITG/bin/ITGSend -Q -l sender.log -x receiver.log &> sender.stdout
+D-ITG/bin/ITGSend -Q -l sender.log -x receiver.log &> sender.stdout &
+sourcesonoff/sourcesonoff --receiver-tcp &
+sourcesonoff/sourcesonoff --receiver-udp
